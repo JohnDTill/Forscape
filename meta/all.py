623a7e3cead7b35f72be040eacfe960ca77edfd8
John Till <johndtill@gmail.com>
2022-01-15 20:02:20 +0000

    Try again

--- meta/all.py
+++ meta/all.py
@@ -27,7 +27,7 @@ def main():
     execfile('errors.py')
     execfile('interpreter_dispatch.py')
     execfile('unicode_scripts.py')
-    os.system("pip install wcwidth")
+    os.system("pip3 install wcwidth")
     execfile('unicode_graphemes.py')
     
     shutil.copyfile("construct_codes.csv", "cache/construct_codes.csv")
