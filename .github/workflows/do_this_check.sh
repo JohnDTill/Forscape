a171159f0fc48629ed0b4930db5c19b8ba5f1e59
John Till <johndtill@gmail.com>
2022-01-16 01:38:50 +0000

    Try again

--- .github/workflows/do_this_check.sh
+++ .github/workflows/do_this_check.sh
@@ -1,6 +1,4 @@
-exc={${GITHUB_WORKSPACE}/src/Eigen, ${GITHUB_WORKSPACE}/src/unsupported}
-
-if grep --exclude=\*.sh --exclude-dir=${exc} -rnw ${GITHUB_WORKSPACE} -i -e "DO THIS"; then
+if grep --exclude=\*.sh --exclude-dir=Eigen -rnw ${GITHUB_WORKSPACE} -i -e "DO THIS"; then
     echo "\nPlease clean up unfinished \"DO THIS\" tasks before merging to main.\n"
     exit 1
 fi
