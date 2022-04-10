7b49b6c3d59ef5e20c2aa18774ddacdc43ae0ca3
John Till <johndtill@gmail.com>
2022-04-10 14:19:02 +0100

    Extract methods

--- src/hope_optimiser.h
+++ src/hope_optimiser.h
@@ -2,6 +2,8 @@
 #define HOPE_OPTIMISER_H
 
 #include <stddef.h>
+#include <unordered_map>
+#include <vector>
 
 namespace Hope {
 
@@ -13,6 +15,15 @@ typedef size_t ParseNode;
 class Optimiser{
 private:
     ParseTree& parse_tree;
+    typedef std::vector<size_t> DimSignature;
+    struct vectorOfIntHash{
+        std::size_t operator()(const std::vector<size_t>& vec) const noexcept {
+            std::size_t seed = vec.size();
+            for(auto& i : vec) seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            return seed;
+        }
+    };
+    std::unordered_map<DimSignature, size_t, vectorOfIntHash> instantiated;
 
 public:
     Optimiser(ParseTree& parse_tree);
@@ -20,6 +31,12 @@ public:
 
 private:
     ParseNode visitNode(ParseNode pn);
+    ParseNode visitDecimal(ParseNode pn);
+    ParseNode visitLength(ParseNode pn);
+    ParseNode visitMult(ParseNode pn);
+    ParseNode visitPower(ParseNode pn);
+    ParseNode visitUnaryMinus(ParseNode pn);
+    ParseNode visitCall(ParseNode pn);
 };
 
 }
