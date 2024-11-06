8f5a20a5e0b270fd4166953843c8bdef32fe66c4
John Till <johndtill@gmail.com>
2024-11-06 22:27:54 +0000

    Ofc it isn't a normal env var
    That would make too much sense

--- app/installer/createInstaller.bat
+++ app/installer/createInstaller.bat
@@ -12,3 +12,4 @@ mkdir "packages\com.automath.forscape\data"
 robocopy ..\..\build\Release packages\com.automath.forscape\data Forscape.exe *.dll /S
 copy ..\lambda.ico config
 copy "..\..\LICENSE" "packages\com.automath.forscape\meta"
+binarycreator.exe --verbose --offline-only -c config/config.xml -p packages ForscapeInstaller_Win64.exe
