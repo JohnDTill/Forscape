6bc74d65d7fff177c8ede6752fa5612cbda4f6ea
John Till <johndtill@gmail.com>
2024-11-07 23:20:59 +0000

    Patch conan?
    I don't even know it's "as: unrecognized option '-mbig-obj'" in release builds

--- .github/workflows/source_instructions.sh
+++ .github/workflows/source_instructions.sh
@@ -1,6 +1,7 @@
 # Compile
 apt-get install cmake python3 qtbase5-dev libqt5svg5-dev
 pip3 install -v "conan==1.65.0"
+conan profile new default --detect && conan profile update settings.compiler.libcxx=libstdc++11 default
 git clone https://github.com/JohnDTill/Forscape
 cd ./Forscape
 conan install --generator cmake_find_package --install-folder ./build/conan-dependencies ./app
