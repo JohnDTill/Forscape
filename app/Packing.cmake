a424ec1a85759409c58edaa53db1aa0799f0c27f
John Till <johndtill@gmail.com>
2024-11-07 20:06:15 +0000

    Add Packing.cmake

---
+++ app/Packing.cmake
@@ -0,0 +1,48 @@
+# these are cache variables, so they could be overwritten with -D,
+set(CPACK_PACKAGE_NAME "forscape"
+    CACHE STRING "Forscape"
+)
+# which is useful in case of packing only selected components instead of the whole thing
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Language and editor for scientific computation"
+    CACHE STRING "Write and execute code with mathematical syntax."
+)
+set(CPACK_PACKAGE_VENDOR "AutoMath")
+
+set(CPACK_VERBATIM_VARIABLES YES)
+
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+
+set(CPACK_PACKAGE_INSTALL_DIRECTORY ${CPACK_PACKAGE_NAME})
+SET(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_SOURCE_DIR}/_packages")
+
+# https://unix.stackexchange.com/a/11552/254512
+# set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/forscape")#/${CMAKE_PROJECT_VERSION}")
+set(CPACK_PACKAGING_INSTALL_PREFIX "/usr")
+set(CMAKE_INSTALL_PREFIX "/usr")
+SET(CPACK_PACKAGE_DEFAULT_LOCATION "/usr")
+
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+
+set(CPACK_PACKAGE_CONTACT "JohnDTill@gmail.com")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "John Till")
+
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/../LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/../README.md")
+
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "cmake, python3, qt5-default, libqt5svg5-dev")
+#qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools DO THIS
+
+# package name for deb
+# if set, then instead of some-application-0.9.2-Linux.deb
+# you'll get some-application_0.9.2_amd64.deb (note the underscores too)
+set(CPACK_DEBIAN_FILE_NAME forscape.deb)
+# if you want every group to have its own package,
+# although the same happens if this is not sent (so it defaults to ONE_PER_GROUP)
+# and CPACK_DEB_COMPONENT_INSTALL is set to YES
+set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)#ONE_PER_GROUP)
+# without this you won't be able to pack only specified component
+set(CPACK_DEB_COMPONENT_INSTALL YES)
+
+include(CPack)
