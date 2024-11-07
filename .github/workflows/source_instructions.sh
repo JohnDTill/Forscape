35afe209cac3abe5500a0824d6c7a27ee64e3560
John Till <johndtill@gmail.com>
2024-11-07 22:17:37 +0000

    Remove compiler flag

--- .github/workflows/source_instructions.sh
+++ .github/workflows/source_instructions.sh
@@ -4,9 +4,15 @@ pip3 install -v "conan==1.65.0"
 git clone https://github.com/JohnDTill/Forscape
 cd ./Forscape
 conan install --generator cmake_find_package --install-folder ./build/conan-dependencies ./app
-cmake -B build -S app --config Release
+cmake -DCMAKE_BUILD_TYPE=Release -B build -S app
 cd build
 make
 
 # Run
-#./Forscape
+# ./Forscape
+
+# Test launch with timed close
+timeout 10 ./Forscape -platform offscreen
+if [ $? == 124 ]; then
+    exit 0
+fi
