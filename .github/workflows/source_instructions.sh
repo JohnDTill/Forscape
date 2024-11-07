ec387ee58308964438429add9e99a799dc4c19e0
John Till <johndtill@gmail.com>
2024-11-07 22:02:39 +0000

    Try again

--- .github/workflows/source_instructions.sh
+++ .github/workflows/source_instructions.sh
@@ -1,9 +1,9 @@
 # Compile
-apt-get install cmake python3 qt5-default qtbase5-dev, qtchooser, qt5-qmake, qtbase5-dev-tools, libqt5svg5-dev
+apt-get install cmake python3 qtbase5-dev libqt5svg5-dev
 pip3 install conan
 git clone https://github.com/JohnDTill/Forscape
 cd ./Forscape
-conan install --generator cmake_find_package --install-folder build/conan-dependencies app
+conan install --generator cmake_find_package --install-folder ./build/conan-dependencies ./app
 cmake -DCMAKE_BUILD_TYPE=Release -B build -S app
 cd build
 make
